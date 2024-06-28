91e329992505c2168483b075f9e1504cf471b562
arezooebrahimi <aeo.ebrahimi@gmail.com>
2024-06-28 13:18:49 +0330

    فصل ۳ - جلسه ۱ - ساختار بندی سرویس Catalog

---
+++ E-Commerce-Microservices/Catalog.API/Controllers/v1/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
