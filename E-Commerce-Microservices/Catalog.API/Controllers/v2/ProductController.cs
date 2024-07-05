03cb3164ff2c6366da94634970cf763103487851
arezooebrahimi <aeo.ebrahimi@gmail.com>
2024-07-05 09:38:59 +0330

    C3 - S6 - Add Swagger

---
+++ E-Commerce-Microservices/Catalog.API/Controllers/v2/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Asp.Versioning;
+using Catalog.API.WebFramework.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers.v2
+{
+    [ApiVersion(2)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public ApiResult<string> Get()
+        {
+            //var y = 0;
+            //var x = 5 / y;
+            return Ok("get all products");
+        }
+
+        [HttpGet("[action]")]
+        public ApiResult<string> GetById()
+        {
+            return Ok("get product by id");
+        }
+
+        [HttpPost("[action]")]
+        public ApiResult<string> Create()
+        {
+            return Ok("create a new product");
+        }
+
+
+        [HttpPut("[action]")]
+        public ApiResult<string> Update()
+        {
+            return Ok("update product by id");
+        }
+
+
+        [HttpDelete("[action]")]
+        public ApiResult<string> Delete()
+        {
+            return Ok("delete product by id");
+        }
+
+    }
+}
--- E-Commerce-Microservices/Catalog.API/Program.cs
+++ E-Commerce-Microservices/Catalog.API/Program.cs
@@ -1,8 +1,31 @@
 using Asp.Versioning;
 using Catalog.API.Middlewares;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "E-Commerce API",
+        Description = "Catalog Service API",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
 builder.Services.AddApiVersioning(setup =>
 {
     setup.DefaultApiVersion = new ApiVersion(1, 0);
@@ -13,6 +36,16 @@ builder.Services.AddApiVersioning(setup =>
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseCustomExceptionHandler();
 app.MapControllers();
