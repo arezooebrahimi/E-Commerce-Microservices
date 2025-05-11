f7e571d92368b3b974eeb794ff3943f2e3e04802
arezooebrahimi <aeo.ebrahimi@gmail.com>
2025-05-11 19:23:48 +0330

    C6 - S03 - Add GRPC Client

---
+++ E-Commerce-Microservices/Admin/Services/Grpc/FileManagerGrpcClient.cs
@@ -0,0 +1,38 @@
+﻿using FileManager.Grpc;
+using Grpc.Net.Client;
+
+namespace Admin.Services.Grpc
+{
+    public class FileManagerGrpcClient
+    {
+        private readonly FileService.FileServiceClient _client;
+
+        public FileManagerGrpcClient()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:44344");
+            _client = new FileService.FileServiceClient(channel);
+        }
+
+        public async Task<List<string>> UploadFilesAsync(List<IFormFile> files)
+        {
+            var request = new UploadFilesRequest();
+
+            foreach (var file in files)
+            {
+                using var ms = new MemoryStream();
+                await file.CopyToAsync(ms);
+                var fileItem = new FileItem
+                {
+                    FileName = file.FileName,
+                    ContentType = file.ContentType,
+                    FileData = Google.Protobuf.ByteString.CopyFrom(ms.ToArray())
+                };
+                request.Files.Add(fileItem);
+            }
+
+            var response = await _client.UploadFilesAsync(request);
+
+            return response.FileIds.ToList();
+        }
+    }
+}
