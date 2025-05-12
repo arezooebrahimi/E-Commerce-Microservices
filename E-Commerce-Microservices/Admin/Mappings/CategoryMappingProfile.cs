96806a80b96d4d5ae05219a7b50ff96ab1af5337
arezooebrahimi <aeo.ebrahimi@gmail.com>
2025-05-12 10:13:28 +0330

    C6 - S04 - Add Mapper Config For Admin Services

--- E-Commerce-Microservices/Admin/Mappings/CategoryMappingProfile.cs
+++ E-Commerce-Microservices/Admin/Mappings/CategoryMappingProfile.cs
@@ -1,6 +1,11 @@
 using AutoMapper;
 using Common.Dtos.Admin.Category;
+using Common.Dtos.Catalog.Brand;
 using Common.Dtos.Catalog.Category;
+using Common.Dtos.Catalog.Feature;
+using Common.Dtos.Catalog.FeatureOption;
+using Common.Dtos.Catalog.Product;
+using Common.Dtos.Catalog.Tag;
 using Common.Entities;
 
 namespace Admin.Mappings;
@@ -10,9 +15,25 @@ public class CategoryMappingProfile : Profile
     public CategoryMappingProfile()
     {
         CreateMap<Category, GetCategoriesPaginateDto>();
+        CreateMap<Tag, GetTagsPaginateDto>();
+        CreateMap<Brand, GetBrandsPaginateDto>();
+        CreateMap<Product, GetProductsPaginateDto>();
+        CreateMap<Feature, GetFeaturesPaginateDto>();
+        CreateMap<FeatureOption, GetFeatureOptionsPaginateDto>();
+
         CreateMap<CreateCategoryRequest, Category>().
             ForMember(dest => dest.Medias, opt => opt.Ignore()).
             ForMember(dest => dest.ImageIdOnHomePage, opt => opt.Ignore());
 
+
+        CreateMap<CreateTagRequest, Tag>().
+            ForMember(dest => dest.Medias, opt => opt.Ignore());
+
+        CreateMap<CreateProductRequest, Product>().
+            ForMember(dest => dest.Medias, opt => opt.Ignore());
+
+        CreateMap<CreateFeatureRequest, Feature>();
+        CreateMap<CreateFeatureOptionRequest, FeatureOption>();
+        CreateMap<CreateBrandRequest, Brand>();
     }
 } 
